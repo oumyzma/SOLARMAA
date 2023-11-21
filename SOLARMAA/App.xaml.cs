4a0cb311d4349fe975ea988fde086d59e9acab00
Omayma Rabhi <omayma.rabhi.76@gmail.com>
2023-11-21 15:06:12 +0100

    coucou 1er commit

---
+++ SOLARMAA/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SOLARMAA
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
