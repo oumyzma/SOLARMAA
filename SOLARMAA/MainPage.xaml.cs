a45e320a83df24ffa27e75928a2ac2d9760faafd
TomB <tombillet@hotmail.com>
2023-11-22 12:00:27 +0100

    inclinaison

--- SOLARMAA/MainPage.xaml.cs
+++ SOLARMAA/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace SOLARMAA
+﻿using System;
+using System.Numerics;
+namespace SOLARMAA
 {
     public partial class MainPage : ContentPage
     {
@@ -7,6 +9,8 @@
         public MainPage()
         {
             InitializeComponent();
+            
+            OrientationSensor.ReadingChanged += OnOrientationSensorReadingChanged;
         }
 
         private void OnClicked(object sender, EventArgs e)
@@ -16,5 +20,50 @@
             currentValue += 0.1;
             progressBar.Progress = currentValue;
         }
+        
+        private void OnOrientationSensorReadingChanged(object sender, OrientationSensorChangedEventArgs e)
+        {
+            if (e.Reading != null)
+            {
+                // Obtenez l'inclinaison du téléphone en degrés
+                double inclinationDegrees = GetInclinationDegrees(e.Reading.Orientation);
+
+                // Mettez à jour l'étiquette avec l'inclinaison actuelle
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    InclinationLabel.Text = $"{inclinationDegrees:F2}°";
+                });
+            }
+        }
+
+        private double GetInclinationDegrees(Quaternion quaternion)
+        {
+            // Convertir le quaternion en matrice de rotation
+            Matrix4x4 rotationMatrix = Matrix4x4.CreateFromQuaternion(quaternion);
+
+            // Extraire l'angle d'inclinaison de la matrice de rotation
+            double inclinationRadians = Math.Asin(rotationMatrix.M23);
+            double inclinationDegrees = inclinationRadians * (180.0 / Math.PI);
+
+            return inclinationDegrees;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // Démarrer l'écoute de l'orientation lorsque la page est affichée
+            OrientationSensor.Start(SensorSpeed.UI);
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            // Arrêter l'écoute de l'orientation lorsque la page n'est plus affichée
+            OrientationSensor.Stop();
+        }
+        
+        
     }
 }
