56394cf2fcc2da5c23ee73b9e43b67a3afe43ecd
Omayma Rabhi <omayma.rabhi.76@gmail.com>
2023-11-23 11:51:19 +0100

    gps

--- SOLARMAA/MainPage.xaml.cs
+++ SOLARMAA/MainPage.xaml.cs
@@ -17,11 +17,6 @@ namespace SOLARMAA
             Gps _gps = new Gps();
             var a = await _gps.GetCachedLocation();
             await DisplayAlert("alert", a, "OK");
-
-            ProgressBar progressBar = this.FindByName<ProgressBar>("myProgressBar");
-            double currentValue = progressBar.Progress;
-            currentValue += 0.1;
-            progressBar.Progress = currentValue;
         }
     }
 }
