8890ffde61e9ce050ad6baf2c1e72d282d797c9b
TomB <tombillet@hotmail.com>
2023-11-28 12:56:43 +0100

    Localisation, Inclinaison tout fonctionne

---
+++ SOLARMAA/Gps.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLARMAA.Services
+{
+    public class Gps
+    {
+        private CancellationTokenSource _cancelTokenSource;
+        private bool _isCheckingLocation;
+
+        public async Task<String> GetCurrentLocation()
+        {
+            try
+            {
+                _isCheckingLocation = true;
+
+                GeolocationRequest request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+
+                _cancelTokenSource = new CancellationTokenSource();
+
+                Location location = await Geolocation.Default.GetLocationAsync(request, _cancelTokenSource.Token);
+
+                if (location != null)
+                    return $"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}";
+            }
+            // Catch one of the following exceptions:
+            //   FeatureNotSupportedException
+            //   FeatureNotEnabledException
+            //   PermissionException
+            catch (Exception ex)
+            {
+                // Unable to get location
+                return "Erreur";
+            }
+            finally
+            {
+                _isCheckingLocation = false;
+            }
+            return "Erreur2";
+        }
+
+        public void CancelRequest()
+        {
+            if (_isCheckingLocation && _cancelTokenSource != null && _cancelTokenSource.IsCancellationRequested == false)
+                _cancelTokenSource.Cancel();
+        }
+    }
+}
--- SOLARMAA/MainPage.xaml.cs
+++ SOLARMAA/MainPage.xaml.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Numerics;
+using SOLARMAA.Services;
 namespace SOLARMAA
+
 {
     public partial class MainPage : ContentPage
     {
@@ -12,13 +16,51 @@ namespace SOLARMAA
             OrientationSensor.ReadingChanged += OnOrientationSensorReadingChanged;
         }
 
+
         protected override async void OnAppearing()
         {
             base.OnAppearing();
+            // Démarrer l'écoute de l'orientation lorsque la page est affichée
+            OrientationSensor.Start(SensorSpeed.UI);
             Gps _gps = new Gps();
             var a = await _gps.GetCurrentLocation();
             await DisplayAlert("alert", a, "OK");
+
+
+        }
+        private void OnOrientationSensorReadingChanged(object sender, OrientationSensorChangedEventArgs e)
+        {
+            if (e.Reading != null)
+            {
+                // Obtenez l'inclinaison du téléphone en degrés
+                double inclinationDegrees = GetInclinationDegrees(e.Reading.Orientation);
+
+                // Mettez à jour l'étiquette avec l'inclinaison actuelle
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    InclinationLabel.Text = $"{inclinationDegrees:F2}°";
+                });
+            }
         }
 
+        private double GetInclinationDegrees(Quaternion quaternion)
+        {
+            // Convertir le quaternion en matrice de rotation
+            Matrix4x4 rotationMatrix = Matrix4x4.CreateFromQuaternion(quaternion);
+
+            // Extraire l'angle d'inclinaison de la matrice de rotation
+            double inclinationRadians = Math.Asin(rotationMatrix.M23);
+            double inclinationDegrees = inclinationRadians * (180.0 / Math.PI);
+
+            return inclinationDegrees;
+        }
+        
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            // Arrêter l'écoute de l'orientation lorsque la page n'est plus affichée
+            OrientationSensor.Stop();
+        }
     }
 }
