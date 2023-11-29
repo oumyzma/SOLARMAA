670c923616fec3c7a326166d0bf125c2f5b17efc
enzotaffin@orange.fr <ETA_sio22@ETA-SIO22>
2023-11-29 10:36:10 +0100

    Fin Affichage

--- SOLARMAA/MainPage.xaml.cs
+++ SOLARMAA/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Numerics;
+using Android.Telephony;
 namespace SOLARMAA
 
 {
@@ -57,15 +58,10 @@ namespace SOLARMAA
             // Extraire l'angle d'inclinaison de la matrice de rotation
             double inclinationRadians = Math.Asin(rotationMatrix.M23);
             double inclinationDegrees = inclinationRadians * (180.0 / Math.PI);
-
+            Affichage(inclinationDegrees);
             return inclinationDegrees;
         }
-        private void OnOrientationSensorReadingChanged(object sender, OrientationSensorChangedEventArgs e)
-        {
-            if (e.Reading != null)
-            {
-                // Obtenez l'inclinaison du téléphone en degrés
-                double inclinationDegrees = GetInclinationDegrees(e.Reading.Orientation);
+
 
         protected override void OnDisappearing()
         {
@@ -73,6 +69,60 @@ namespace SOLARMAA
 
             // Arrêter l'écoute de l'orientation lorsque la page n'est plus affichée
             OrientationSensor.Stop();
+        }
+        async private void Affichage(double inclinationDegrees)
+        {
+            PourcentageHome.Text = $"{inclinationDegrees/90*100:F0}";
+            await ProgressBarHome.ProgressTo(inclinationDegrees / 90, 300, Easing.Linear);
+            if (inclinationDegrees > 80)
+            {
+                ProgressBarHome.ProgressColor = Colors.Cyan;
+                TexteRendement.Text = "Votre emplacement est parfait !";
+                TexteRendement.TextColor = Colors.Cyan;
+            }
+            else if (inclinationDegrees > 70)
+            {
+                ProgressBarHome.ProgressColor = Colors.LightGreen;
+                TexteRendement.Text = "Votre emplacement est presque parfait !";
+                TexteRendement.TextColor = Colors.LightGreen;
+
+            }
+            else if (inclinationDegrees > 60)
+            {
+                ProgressBarHome.ProgressColor = Colors.YellowGreen;
+                TexteRendement.Text = "Votre emplacement est bien !";
+                TexteRendement.TextColor = Colors.YellowGreen;
+
+            }
+            else if (inclinationDegrees > 45)
+            {
+                ProgressBarHome.ProgressColor = Colors.Yellow;
+                TexteRendement.Text = "Votre emplacement est pas super";
+                TexteRendement.TextColor = Colors.Yellow;
+
+            }
+            else if (inclinationDegrees > 30)
+            {
+                ProgressBarHome.ProgressColor = Colors.Orange;
+                TexteRendement.Text = "Votre emplacement est pas rentable";
+
+                TexteRendement.TextColor = Colors.Orange;
+            }
+            else if (inclinationDegrees > 15)
+            {
+                ProgressBarHome.ProgressColor = Colors.OrangeRed;
+                TexteRendement.Text = "Votre emplacement est nul";
+                TexteRendement.TextColor = Colors.OrangeRed;
+
+            }
+            else
+            {
+                ProgressBarHome.ProgressColor = Colors.Red;
+                TexteRendement.Text = "Votre emplacement est super nul";
+                TexteRendement.TextColor = Colors.Red;
+
+            }
+
         }
     }
 }
